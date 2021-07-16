33fa40c6ad59245f3d527ec43eaffab12d4a061c
Ivan Klimchuk <ivan.klimchuk@1pt.com>
2021-07-16 18:13:01 +0300

    fix: improve view of the block with qr-code

--- assets/mspoplati/app/oplati.app.js
+++ assets/mspoplati/app/oplati.app.js
@@ -10,54 +10,45 @@ document
     .querySelectorAll(oWrapper)
     .forEach(block => {
 
-        const oplatiinfomessage = '';
+        // console.log(block.dataset, block.dataset.path.padStart(7, '#'));
 
-
-    console.log(
-        block.dataset.size,
-        block.dataset.fill,
-        block.dataset.path,
-        block.dataset.code,
-    );
-
-    const codeBlock = block.querySelector(oCodeBlock);
-
-    const qrCode = new QRCode(codeBlock, {
-        width: block.dataset.size,
-        height: block.dataset.size,
-        colorDark : block.dataset.path,
-        colorLight : block.dataset.fill,
-        correctLevel : QRCode.CorrectLevel.H,
-    });
-
-    qrCode.clear();
-    qrCode.makeCode(block.dataset.code);
-
-    // setInterval();
-
-    const button = block.querySelector('.button');
-
-    console.log(button);
-
-    button.addEventListener('click', function () {
-
-        const urlParams = new URLSearchParams(window.location.search);
-        const orderId = urlParams.get('msorder');
-
-        let form = new FormData();
-        form.set('ctx', 'web');
-        form.set('action', 'check');
-        form.set('id', orderId);
-
-        fetch("/assets/components/mspoplati/connector.php", {
-            method: "POST",
-            body: form
-        }).then(res => {
-            console.log("Request complete!, resp: ", res);
+        const qrCode = new QRCode(block.querySelector(oCodeBlock), {
+            width: block.dataset.size,
+            height: block.dataset.size,
+            colorDark: block.dataset.path.padStart(7, '#'),
+            colorLight: block.dataset.fill.padStart(7, '#'),
+            correctLevel: QRCode.CorrectLevel.H,
         });
 
-    });
+        qrCode.clear();
+        qrCode.makeCode(block.dataset.code);
+
+        let start = 60;
+        let interval = setInterval(() => {
+            start--;
+            block.querySelector(oTimerBlock).textContent = '00:' + start.toString(10).padStart(2, '0');
+            // every 3 second?
+            // update state?
+            checkPaymentStatus(block.dataset.oid);
+
+            if (start <= 0) {
+                clearInterval(interval);
+                // show button for renew
+                console.log('renewwing the code');
+            }
+        }, 1000);
 });
 
-// check payment
-// request new code
+function checkPaymentStatus(oid) {
+    let form = new FormData();
+    form.set('ctx', 'web');
+    form.set('action', 'check');
+    form.set('id', oid);
+
+    fetch('/assets/components/mspoplati/connector.php', {
+        method: 'POST',
+        body: form
+    }).then(response => {
+        console.log(response);
+    });
+}
