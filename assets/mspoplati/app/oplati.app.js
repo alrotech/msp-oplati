2fe2a506c5afe3bc065e51bc006f2f86c71b325d
Ivan Klimchuk <ivan.klimchuk@1pt.com>
2021-07-19 15:53:43 +0300

    feat: finished frontend application, now it handles statuses

--- assets/mspoplati/app/oplati.app.js
+++ assets/mspoplati/app/oplati.app.js
@@ -6,49 +6,77 @@ const oMobileBlock = '.oplati-mobile-block';
 const oTimerBlock = '.oplati-timer-block';
 const oStatusBlock = '.oplati-status-block';
 
-document
-    .querySelectorAll(oWrapper)
-    .forEach(block => {
+function determineLibraryPath(filename) {
+    const scripts = document.getElementsByTagName("script");
+    const relative = scripts[scripts.length-1].src
+        .replace(window.location.origin, '')
+        .split('/').slice(0, -1).join('/');
 
-        // console.log(block.dataset, block.dataset.path.padStart(7, '#'));
+    return [relative, filename].join('/');
+}
 
-        const qrCode = new QRCode(block.querySelector(oCodeBlock), {
-            width: block.dataset.size,
-            height: block.dataset.size,
-            colorDark: block.dataset.path.padStart(7, '#'),
-            colorLight: block.dataset.fill.padStart(7, '#'),
-            correctLevel: QRCode.CorrectLevel.H,
+function loadQRCodeLibrary(url, callback) {
+    let script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = url;
+
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    document.body.appendChild(script);
+}
+
+function initQRCodeBlock()
+{
+    document
+        .querySelectorAll(oWrapper)
+        .forEach(block => {
+
+            const qrCode = new QRCode(block.querySelector(oCodeBlock), {
+                width: block.dataset.size,
+                height: block.dataset.size,
+                colorDark: block.dataset.path.padStart(7, '#'),
+                colorLight: block.dataset.fill.padStart(7, '#'),
+                correctLevel: QRCode.CorrectLevel.H,
+            });
+
+            qrCode.clear();
+            qrCode.makeCode(block.dataset.code);
+
+            let start = 60;
+            let threshold = 3;
+            let interval = setInterval(() => {
+                if (start < 1) {
+                    clearInterval(interval);
+                    // show button for renew
+                    // console.log('renewwing the code');
+                }
+
+                block.querySelector(oTimerBlock).textContent = '00:' + start.toString(10).padStart(2, '0');
+                start--;
+
+                // every 3 seconds
+                if (start % threshold === 0) {
+                    checkPaymentStatus(block.dataset.oid).then((data) => {
+                        block.querySelector(oStatusBlock).textContent = data['object'].properties.humanStatus;
+                    });
+                }
+            }, 1000);
         });
+}
 
-        qrCode.clear();
-        qrCode.makeCode(block.dataset.code);
-
-        let start = 60;
-        let interval = setInterval(() => {
-            start--;
-            block.querySelector(oTimerBlock).textContent = '00:' + start.toString(10).padStart(2, '0');
-            // every 3 second?
-            // update state?
-            checkPaymentStatus(block.dataset.oid);
-
-            if (start <= 0) {
-                clearInterval(interval);
-                // show button for renew
-                console.log('renewwing the code');
-            }
-        }, 1000);
-});
-
-function checkPaymentStatus(oid) {
+async function checkPaymentStatus(oid) {
     let form = new FormData();
     form.set('ctx', 'web');
     form.set('action', 'check');
     form.set('id', oid);
 
-    fetch('/assets/components/mspoplati/connector.php', {
+    const response = await fetch('/assets/components/mspoplati/connector.php', {
         method: 'POST',
         body: form
-    }).then(response => {
-        console.log(response);
-    });
+    })
+
+    return await response.json();
 }
+
+loadQRCodeLibrary(determineLibraryPath('qrcode.min.js'), initQRCodeBlock);
