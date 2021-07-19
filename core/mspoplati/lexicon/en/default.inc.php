8422eda98855d085c788aacd722f46e46ee71e9b
Ivan Klimchuk <ivan.klimchuk@1pt.com>
2021-07-19 15:51:35 +0300

    feat: translations and human text for payment statuses

--- core/mspoplati/lexicon/en/default.inc.php
+++ core/mspoplati/lexicon/en/default.inc.php
@@ -7,7 +7,14 @@ $_lang = array_merge($_lang, [
 
     'oplati-info-message' => 'Payment for order no. [[+num]]',
     'oplati-help-message' => 'Please, scan the QR-code with the Oplati app.',
-    'oplati-mobile-button-text' => 'Pay via Oplati'
+    'oplati-mobile-button-text' => 'Pay via Oplati',
+
+    'oplati-status-in-progress' => 'Awaiting payment',
+    'oplati-status-ok' => 'Successfully paid',
+    'oplati-status-decline' => 'Payment canceled',
+    'oplati-status-not-enough' => 'Insufficient funds',
+    'oplati-status-timeout' => 'Payment is overdue',
+    'oplati-status-unknown' => 'Unknown status',
 
 ]);
 
--- core/mspoplati/lexicon/ru/default.inc.php
+++ core/mspoplati/lexicon/ru/default.inc.php
@@ -7,7 +7,14 @@ $_lang = array_merge($_lang, [
 
     'oplati-info-message' => 'Оплата заказа №[[+num]]',
     'oplati-help-message' => 'Отсканируйте QR-код приложением "Оплати"',
-    'oplati-mobile-button-text' => 'Оплатить через "Оплати"'
+    'oplati-mobile-button-text' => 'Оплатить через "Оплати"',
+
+    'oplati-status-in-progress' => 'Ожидает оплаты',
+    'oplati-status-ok' => 'Успешно оплачено',
+    'oplati-status-decline' => 'Платеж отменен',
+    'oplati-status-not-enough' => 'Недостаточно средств',
+    'oplati-status-timeout' => 'Платеж просрочен',
+    'oplati-status-unknown' => 'Неизвестный статус',
 
 ]);
 
