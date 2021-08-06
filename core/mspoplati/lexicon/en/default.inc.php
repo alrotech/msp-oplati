1e3732e60f2e9dfd059dfaca9dec7d9515939c36
Ivan Klimchuk <ivan@klimchuk.com>
2021-08-06 11:16:10 +0300

    chore: return normal format of strings

--- core/mspoplati/lexicon/en/default.inc.php
+++ core/mspoplati/lexicon/en/default.inc.php
@@ -1,20 +1,14 @@
 <?php
 
-/** @var Array $_lang */
+/** @var array $_lang */
 
-$_lang = array_merge($_lang, [
-    'oplati' => 'Oplati',
-
-    'oplati-info-message' => 'Payment for order no. [[+num]]',
-    'oplati-help-message' => 'Please, scan the QR-code with the Oplati app.',
-    'oplati-mobile-button-text' => 'Pay via Oplati',
-
-    'oplati-status-in-progress' => 'Awaiting payment',
-    'oplati-status-ok' => 'Successfully paid',
-    'oplati-status-decline' => 'Payment canceled',
-    'oplati-status-not-enough' => 'Insufficient funds',
-    'oplati-status-timeout' => 'Payment is overdue',
-    'oplati-status-unknown' => 'Unknown status',
-
-]);
-
+$_lang['oplati'] = 'Oplati';
+$_lang['oplati-info-message'] = 'Payment for order no. [[+num]]';
+$_lang['oplati-help-message'] = 'Please, scan the QR-code with the Oplati app.';
+$_lang['oplati-mobile-button-text'] = 'Pay via Oplati';
+$_lang['oplati-status-in-progress'] = 'Awaiting payment';
+$_lang['oplati-status-ok'] = 'Successfully paid';
+$_lang['oplati-status-decline'] = 'Payment canceled';
+$_lang['oplati-status-not-enough'] = 'Insufficient funds';
+$_lang['oplati-status-timeout'] = 'Payment is overdue';
+$_lang['oplati-status-unknown'] = 'Unknown status';
--- core/mspoplati/lexicon/en/settings.inc.php
+++ core/mspoplati/lexicon/en/settings.inc.php
@@ -2,52 +2,50 @@
 
 /** @var Array $_lang */
 
-$_lang = array_merge($_lang, [
-    'area_oplati_credentials' => 'Credentials',
-    'area_oplati_gateways' => 'Gateways',
-    'area_oplati_receipt' => 'Receipt',
-    'area_oplati_statuses' => 'Statuses',
-    'area_oplati_pages' => 'Pages',
+$_lang['area_oplati_credentials'] = 'Credentials';
+$_lang['area_oplati_gateways'] = 'Gateways';
+$_lang['area_oplati_receipt'] = 'Receipt';
+$_lang['area_oplati_statuses'] = 'Statuses';
+$_lang['area_oplati_pages'] = 'Pages';
 
-    'setting_oplati_cash_register_number' => 'Cach register number',
-    'setting_oplati_cash_register_number_desc' => '',
+$_lang['setting_oplati_cash_register_number'] = 'Cash register number';
+$_lang['setting_oplati_cash_register_number_desc'] = '';
 
-    'setting_oplati_cash_password' => '',
-    'setting_oplati_cash_password_desc' => '',
+$_lang['setting_oplati_cash_password'] = '';
+$_lang['setting_oplati_cash_password_desc'] = '';
 
-    'setting_oplati_gateway_url' => '',
-    'setting_oplati_gateway_url_desc' => '',
+$_lang['setting_oplati_gateway_url'] = '';
+$_lang['setting_oplati_gateway_url_desc'] = '';
 
-    'setting_oplati_gateway_url_test' => '',
-    'setting_oplati_gateway_url_test_desc' => '',
+$_lang['setting_oplati_gateway_url_test'] = '';
+$_lang['setting_oplati_gateway_url_test_desc'] = '';
 
-    'setting_oplati_developer_mode' => '',
-    'setting_oplati_developer_mode_desc' => '',
+$_lang['setting_oplati_developer_mode'] = '';
+$_lang['setting_oplati_developer_mode_desc'] = '';
 
-    'setting_oplati_title_text' => '',
-    'setting_oplati_title_text_desc' => '',
+$_lang['setting_oplati_title_text'] = '';
+$_lang['setting_oplati_title_text_desc'] = '';
 
-    'setting_oplati_receipt_header_text' => '',
-    'setting_oplati_receipt_header_text_desc' => '',
+$_lang['setting_oplati_receipt_header_text'] = '';
+$_lang['setting_oplati_receipt_header_text_desc'] = '';
 
-    'setting_oplati_receipt_footer_text' => '',
-    'setting_oplati_receipt_footer_text_desc' => '',
+$_lang['setting_oplati_receipt_footer_text'] = '';
+$_lang['setting_oplati_receipt_footer_text_desc'] = '';
 
-    'setting_oplati_print_crn' => '',
-    'setting_oplati_print_crn_desc' => '',
+$_lang['setting_oplati_print_crn'] = '';
+$_lang['setting_oplati_print_crn_desc'] = '';
 
-    'setting_oplati_success_status' => '',
-    'setting_oplati_success_status_desc' => '',
+$_lang['setting_oplati_success_status'] = '';
+$_lang['setting_oplati_success_status_desc'] = '';
 
-    'setting_oplati_failure_status' => '',
-    'setting_oplati_failure_status_desc' => '',
+$_lang['setting_oplati_failure_status'] = '';
+$_lang['setting_oplati_failure_status_desc'] = '';
 
-    'setting_oplati_success_page' => '',
-    'setting_oplati_success_page_desc' => '',
+$_lang['setting_oplati_success_page'] = '';
+$_lang['setting_oplati_success_page_desc'] = '';
 
-    'setting_oplati_failure_page' => '',
-    'setting_oplati_failure_page_desc' => '',
+$_lang['setting_oplati_failure_page'] = '';
+$_lang['setting_oplati_failure_page_desc'] = '';
 
-    'setting_oplati_unpaid_page' => '',
-    'setting_oplati_unpaid_page_desc' => '',
-]);
+$_lang['setting_oplati_unpaid_page'] = '';
+$_lang['setting_oplati_unpaid_page_desc'] = '';
