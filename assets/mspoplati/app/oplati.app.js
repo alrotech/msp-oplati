5078b5ceddb8153186fecf348af02f62174d23c2
Ivan Klimchuk <ivan@klimchuk.com>
2021-08-17 01:45:30 +0300

    feat: special button for updating the code after timeout

--- assets/mspoplati/app/oplati.app.js
+++ assets/mspoplati/app/oplati.app.js
@@ -1,12 +1,17 @@
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-const oWrapper = '.oplati-wrapper';
+const oContainer = 'oplati-container';
 const oCodeBlock = '.oplati-code-block';
-const oInfoMessage = '.oplati-info-message';
-const oHelpMessage = '.oplati-help-message';
-const oMobileBlock = '.oplati-mobile-block';
 const oTimerBlock = '.oplati-timer-block';
 const oStatusBlock = '.oplati-status-block';
+const oRenewBlock = '.oplati-renew-block';
+
+const threshold = 3; // check status every 3 seconds
+
+let timer;
+// let duration = 7; // in seconds
+
+// https://shop.local.docker/orders.html?msorder=148
 
 function determineLibraryPath(filename) {
     const scripts = document.getElementsByTagName("script");
@@ -28,61 +33,121 @@ function loadQRCodeLibrary(url, callback) {
     document.body.appendChild(script);
 }
 
-function initQRCodeBlock()
-{
-    document
-        .querySelectorAll(oWrapper)
-        .forEach(block => {
+let ticker = function(block, code, defduration = 7) {
+    let duration = defduration;
 
-            if (isMobile) {
-                return;
-            }
+    console.log(duration);
 
-            const qrCode = new QRCode(block.querySelector(oCodeBlock), {
-                width: block.dataset.size,
-                height: block.dataset.size,
-                colorDark: block.dataset.path.padStart(7, '#'),
-                colorLight: block.dataset.fill.padStart(7, '#'),
-                correctLevel: QRCode.CorrectLevel.H,
-            });
+    block.querySelector(oTimerBlock).textContent = '00:' + duration.toString().padStart(2, '0');
+    duration--;
 
-            qrCode.clear();
-            qrCode.makeCode(block.dataset.code);
+    console.log(duration);
 
-            let start = 60;
-            let threshold = 3;
-            let interval = setInterval(() => {
-                if (start < 1) {
-                    clearInterval(interval);
-                    // show button for renew
-                    // console.log('renewwing the code');
-                }
 
-                block.querySelector(oTimerBlock).textContent = '00:' + start.toString(10).padStart(2, '0');
-                start--;
-
-                // every 3 seconds
-                if (start % threshold === 0) {
-                    checkPaymentStatus(block.dataset.oid).then((data) => {
-                        block.querySelector(oStatusBlock).textContent = data['object'].properties.humanStatus;
-                    });
-                }
-            }, 1000);
+    // if (start % threshold === 0 && start >= threshold) {
+    if (duration % threshold === 0 || duration < 1) {
+        requestBackend(block.dataset.oid, 'check').then((data) => {
+            block.querySelector(oStatusBlock).textContent = data['object'].properties.humanStatus;
         });
+    }
+
+    if (duration < 1) {
+        clearInterval(timer);
+        hide(block.querySelector(oTimerBlock));
+        show(block.querySelector(oRenewBlock)).addEventListener('click', function (e) {
+            e.preventDefault();
+            requestBackend(block.dataset.oid, 'renew').then((data) => {
+                block.dataset.code = data['object'].properties.dynamicQR;
+
+                hide(block.querySelector(oRenewBlock));
+                show(block.querySelector(oTimerBlock)).textContent = '--:--';
+
+                code.clear();
+                code.makeCode(block.dataset.code);
+
+                console.log('renew code');
+
+                // duration = 7;
+                timer = window.setInterval(ticker, 1000, block, code);
+            })
+        });
+    }
 }
 
-async function checkPaymentStatus(oid) {
+function initQRCodeBlock()
+{
+    if (isMobile) {
+        return;
+    }
+
+    let container = document.getElementById(oContainer);
+
+    const qrCode = new QRCode(empty(container.querySelector(oCodeBlock)), {
+        width: container.dataset.size,
+        height: container.dataset.size,
+        colorDark: container.dataset.path.padStart(7, '#'),
+        colorLight: container.dataset.fill.padStart(7, '#'),
+        correctLevel: QRCode.CorrectLevel.H,
+    });
+
+    qrCode.clear();
+    qrCode.makeCode(container.dataset.code);
+
+    // duration = 7;
+    timer = window.setInterval(ticker, 1000, container, qrCode);
+
+            // let interval = setInterval(() => {
+            //     block.querySelector(oTimerBlock).textContent = '00:' + start.toString(10).padStart(2, '0');
+            //     start--;
+            //
+            //     // if (start % threshold === 0 && start >= threshold) {
+            //     if (start % threshold === 0 || start < 1) {
+            //         requestBackend(block.dataset.oid, 'check').then((data) => {
+            //             block.querySelector(oStatusBlock).textContent = data['object'].properties.humanStatus;
+            //         });
+            //     }
+            //
+            //     if (start < 1) {
+            //         clearInterval(interval);
+            //
+            //         hide(block.querySelector(oTimerBlock));
+            //
+            //         show(block.querySelector(oRenewBlock)).addEventListener('click', function (e) {
+            //             e.preventDefault();
+            //             requestBackend(block.dataset.oid, 'renew').then((data) => {
+            //                 block.dataset.code = data['object'].properties.dynamicQR;
+            //                 // initQRCodeBlock();
+            //
+            //                 hide(block.querySelector(oRenewBlock));
+            //                 show(block.querySelector(oTimerBlock)).textContent = '--:--';
+            //
+            //                 qrCode.clear();
+            //                 qrCode.makeCode(block.dataset.code);
+            //
+            //                 // simple reset values, no more
+            //             })
+            //         });
+            //     }
+            // }, 1000);
+        // });
+}
+
+async function requestBackend(oid, action, context = 'web') {
     let form = new FormData();
-    form.set('ctx', 'web');
-    form.set('action', 'check');
+    form.set('ctx', context);
+    form.set('action', action);
     form.set('id', oid);
 
     const response = await fetch('/assets/components/mspoplati/connector.php', {
         method: 'POST',
         body: form
-    })
+    });
 
     return await response.json();
 }
 
 loadQRCodeLibrary(determineLibraryPath('qrcode.min.js'), initQRCodeBlock);
+
+const show = function (elem) { elem.style.display = 'block'; return elem; };
+const hide = function (elem) { elem.style.display = 'none';  return elem; };
+const empty = function (elem) { elem.textContent = ''; return elem; }
--- core/mspoplati/elements/chunks/qrcode.tpl
+++ core/mspoplati/elements/chunks/qrcode.tpl
@@ -1,4 +1,4 @@
-<div class="oplati-wrapper" data-size="[[+size]]" data-fill="[[+fill]]" data-path="[[+path]]" data-code="[[+code]]" data-oid="[[+oid]]">
+<div id="oplati-container" class="oplati-wrapper" data-size="[[+size]]" data-fill="[[+fill]]" data-path="[[+path]]" data-code="[[+code]]" data-oid="[[+oid]]">
 
     <span class="oplati-info-message">[[%oplati-info-message? &num=`[[+num]]`]]</span>
     <div class="oplati-code-block"></div>
@@ -8,6 +8,17 @@
         <a href="https://getapp.o-plati.by/map/?app_link=[[+code]]" class="oplati-mobile-button">[[%oplati-mobile-button-text]]</a>
     </div>
 
-    <div class="oplati-timer-block">00:60</div>
+    <div class="oplati-timer-block">--:--</div>
+
+    <div class="oplati-renew-block">
+        <button class="oplati-renew-button">
+            <svg height="1rem" width="1rem" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.004 512.004">
+                <path d="M508.034 133.475l-112-128c-6.08-6.944-18.016-6.944-24.096 0l-112 128c-4.128 4.704-5.12 11.424-2.528 17.152 2.624 5.728 8.32 9.376 14.592 9.376h48v186.176c0 41.408-14.24 82.016-40.096 114.368l-20.384 25.472c-4.192 5.248-4.672 12.576-1.184 18.304 2.944 4.832 8.16 7.68 13.664 7.68 1.024 0 2.08-.096 3.136-.32 100.16-20.032 172.864-108.672 172.864-210.848V160.003h48c6.272 0 11.968-3.648 14.56-9.376 2.624-5.728 1.6-12.448-2.528-17.152zm-76.032-5.472c-8.832 0-16 7.168-16 16v156.832c0 69.376-39.392 131.456-98.656 162.368 22.464-34.656 34.656-75.52 34.656-117.024V144.003c0-8.832-7.168-16-16-16h-28.736l76.736-87.68 76.736 87.68h-28.736z"/>
+                <path d="M240.002 352.003h-48V165.827c0-41.408 14.24-82.016 40.096-114.368l20.384-25.44c4.192-5.28 4.704-12.576 1.184-18.336-3.488-5.76-10.08-8.704-16.8-7.36C136.706 20.355 64.002 108.995 64.002 211.171v140.832h-48c-6.272 0-11.968 3.648-14.56 9.376-2.624 5.728-1.6 12.448 2.528 17.152l112 128c3.04 3.488 7.424 5.472 12.032 5.472s8.992-2.016 12.032-5.472l112-128c4.128-4.704 5.12-11.424 2.528-17.152-2.592-5.728-8.288-9.376-14.56-9.376zm-112 119.68l-76.736-87.68h28.736c8.832 0 16-7.168 16-16V211.171c0-69.376 39.392-131.488 98.656-162.4-22.464 34.688-34.656 75.552-34.656 117.056v202.176c0 8.832 7.168 16 16 16h28.736l-76.736 87.68z"/>
+            </svg>
+            <span>[[%oplati-renew-button-text]]</span>
+        </button>
+    </div>
+
     <div class="oplati-status-block">[[%oplati-status-in-progress]]</div>
 </div>
